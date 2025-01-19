410310928327458d8125a63dfd2c9c55e4e6604c
Andrei Florea <andreiflorea@dyn-10-112-1-14-guest.wless.uoregon.edu>
2025-01-18 16:09:43 -0800

    Working on adding create account page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Box, Text } from "@chakra-ui/react";
+import { CreateAccountPage } from "./CreateAccountPage";
 
 function App() {
   return (
