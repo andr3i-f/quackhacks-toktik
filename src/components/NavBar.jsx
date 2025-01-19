2124514b3f3e366f6ef47d097fd9b79f4ec2d448
abd00l4h <114624309+abd00l4h@users.noreply.github.com>
2025-01-19 10:40:48 -0800

    make gui consistent and updated the logo navbar to have the text also link to home

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,8 +13,8 @@ function NavBar() {
       bg="gray.100"
       gap={2}
     >
-      <Flex alignItems="center" gap={2}>
-        <Link href="/home">
+      <Link href="/home">
+        <Flex alignItems="center" gap={2}>
           <IconButton
             aria-label="TokTik Icon"
             icon={
@@ -27,9 +27,11 @@ function NavBar() {
             fontSize="48px"
             variant="ghost"
           />
-        </Link>
-        <Text textStyle="7x1" fontWeight="bold">TokTik</Text>
-      </Flex>
+          <Text textStyle="7x1" fontWeight="bold">
+            TokTik
+          </Text>
+        </Flex>
+      </Link>
 
       <Flex gap={2}>
         <Link href="/upload" passhref legacyBehavior>
