07afce47440e8a0ba1b184ab08d83ae3adb33059
andr3i-f <105520459+andr3i-f@users.noreply.github.com>
2025-01-19 10:39:49 -0800

    Revert "Adding get gif"

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,44 +1,26 @@
-import { Text, Alert, AlertIcon, Box, Button, FormControl, FormLabel, Input, Heading, Spinner, Fade } from "@chakra-ui/react";
+import { Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Heading,
+} from "@chakra-ui/react";
 import NavBar from "../components/NavBar";
 import { loginUser } from "../api/loginUser";
-import { useContext, useState } from "react";
-import { motion } from "framer-motion";
-import { AuthContext } from "../AuthContext";
-import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [status, setStatus] = useState(null); // null, "success", or "error"
-  const [loading, setLoading] = useState(false);
-  const { login } = useContext(AuthContext);
-  const navigate = useNavigate();
+
   const onSubmit = async (event) => {
     event.preventDefault();
-    setLoading(true);
     if (email && password) {
-      await loginUser(email, password)
-        .then((response) => {
-          if (response.success) {
-            console.log(response)
-            setStatus("success");
-            login(response.name)
-            navigate("/player")
-
-          } else {
-            setStatus("error");
-          }
-        })
-        .catch((error) => {
-          console.error("Error during API call:", error);
-          setStatus("error");
-        })
-        .finally(() => {
-          setLoading(false);
-        });
+      await loginUser(email, password);
     }
   };
-
   return (
     <>
       <NavBar />
@@ -79,45 +61,10 @@ export function LoginPage() {
                 placeholder="Enter your password"
               />
             </FormControl>
-            <Button
-              colorScheme="blue"
-              width="100%"
-              type="submit"
-              isLoading={loading}
-            >
+            <Button colorScheme="blue" width="100%" type="submit">
               Login
             </Button>
           </form>
-          <Box mt={4}>
-            {status === "success" && (
-              <Fade in>
-                <Alert
-                  status="success"
-                  variant="subtle"
-                  flexDirection="column"
-                  alignItems="center"
-                  justifyContent="center"
-                  textAlign="center"
-                  height="200px"
-                  borderRadius="lg"
-                >
-                  <AlertIcon boxSize="40px" mr={0} />
-                  <Heading as="h4" size="md" mt={4} mb={1}>
-                    Login Successful!
-                  </Heading>
-                  <Text>Welcome back! Redirecting...</Text>
-                </Alert>
-              </Fade>
-            )}
-            {status === "error" && (
-              <Fade in>
-                <Alert status="error" borderRadius="lg">
-                  <AlertIcon />
-                  Incorrect email or password. Please try again.
-                </Alert>
-              </Fade>
-            )}
-          </Box>
         </Box>
       </Box>
     </>
