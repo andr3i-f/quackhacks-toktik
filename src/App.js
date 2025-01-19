977db5f9af8bd63f252681c20890e228ea215bf2
MujtabaAliProjects <mujtaba.i1@yahoo.com>
2025-01-18 17:58:19 -0800

    Properly moved upstream with video centered and bg black

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { Box, Text } from "@chakra-ui/react";
-import { LandingPage } from './LandingPage';
+import VideoPlayer from "./VideoPlayer";
 
 
 function App() {
   return (
-    <LandingPage />
+    <VideoPlayer />
   );
 }
 
