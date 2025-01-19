d5f1a12e6b0db0719e385f14c5a69d44964c007c
Andrei Florea <andreiflorea@dyn-10-112-1-14-guest.wless.uoregon.edu>
2025-01-18 16:09:43 -0800

    Working on adding create account page

--- src/CreateAccountPage.jsx
+++ src/CreateAccountPage.jsx
@@ -1,24 +1,33 @@
 import { Box, Text, Heading, FormControl, FormLabel, Input, Button } from "@chakra-ui/react";
 
 export function CreateAccountPage() {
-    return (<Box>
-        <Heading>Create Account for TokTik</Heading>
-         <form>
-            <FormControl mb={4}>
-            <FormLabel>Email</FormLabel>
-            <Input type="email" placeholder="Enter your email" />
-            </FormControl>
-            <FormControl mb={4}>
-            <FormLabel>Username</FormLabel>
-            <Input type="email" placeholder="Enter your username" />
-            </FormControl>
-            <FormControl mb={4}>
-            <FormLabel>Password</FormLabel>
-            <Input type="password" placeholder="Enter your password" />
-            </FormControl>
-            <Button colorScheme="blue" width="100%" type="submit">
-            Login
-            </Button>
-        </form>
+    const onSubmit = () => {
+        console.log('test')
+    }
+    return (<Box display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+        bg="gray.100" >
+        <Box>
+            <Heading>Create Account for TokTik</Heading>
+            <form onSubmit={onSubmit}>
+                <FormControl mb={4}>
+                    <FormLabel>Email</FormLabel>
+                    <Input type="email" placeholder="Enter your email" />
+                </FormControl>
+                <FormControl mb={4}>
+                    <FormLabel>Username</FormLabel>
+                    <Input type="email" placeholder="Enter your username" />
+                </FormControl>
+                <FormControl mb={4}>
+                    <FormLabel>Password</FormLabel>
+                    <Input type="password" placeholder="Enter your password" />
+                </FormControl>
+                <Button colorScheme="blue" width="100%" type="submit">
+                    Create Account
+                </Button>
+            </form>
+        </Box>
     </Box>)
 }
