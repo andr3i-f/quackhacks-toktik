2e3e270276e141a79499d4674842369bf0dc15ed
M005A <machomusa1@gmail.com>
2025-01-19 05:16:53 -0800

    BRUHHH scroll page

--- src/Config.js
+++ src/Config.js
@@ -1,2 +1,2 @@
-export const BACKEND = 'http://localhost:5001/api'
+export const BACKEND = 'http://localhost:5000/api'
 
--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,172 +1,165 @@
-import { Text, Box, Image, Center, IconButton, VStack, HStack, Flex } from '@chakra-ui/react';
-import { AddIcon, ArrowDownIcon, ArrowUpIcon } from '@chakra-ui/icons';
+import {
+  Text,
+  Box,
+  Image,
+  Center,
+  IconButton,
+  VStack,
+  Flex,
+} from "@chakra-ui/react";
+import { ArrowDownIcon, ArrowUpIcon } from "@chakra-ui/icons";
 import { RiThumbUpFill, RiThumbDownFill } from "react-icons/ri";
 import { MdOutlineComment } from "react-icons/md";
-import { useEffect, useState } from 'react';
-import { getGifIndex } from '../api/gifGetIndex';
+import { useEffect, useState } from "react";
+import { getGifIndex } from "../api/gifGetIndex";
 
-async function getGifsFunc(index, setFileData) {
-    try {
-        const data = await getGifIndex(index);
-        setFileData(data.gifData)
-    } catch (error) {
-        console.error("Error fetching GIF:", error);
-    }
+async function getGifsFunc(index, setData) {
+  try {
+    const data = await getGifIndex(index);
+    setData(data);
+  } catch (error) {
+    console.error("Error fetching GIF:", error);
+  }
 }
 
 export function VideoPlayer() {
-    const [index, setIndex] = useState(0)
-    const [fileData, setFileData] = useState(null);
+  const [index, setIndex] = useState(0);
+  const [fileData, setData] = useState(null);
 
-    useEffect(() => {
-        getGifsFunc(index, setFileData)
-    }, [index])
+  useEffect(() => {
+    getGifsFunc(index, setData);
+  }, [index]);
 
-    console.log(fileData)
+  return (
+    <Box
+      w="100%"
+      h="100vh"
+      bgGradient="linear(to-b, black, gray.900)"
+      position="relative"
+    >
+      {/* Video Content */}
+      <Center h="100%">
+        <Flex align="center" justify="center" w="80%" maxW="500px" gap="5">
+          {/* Video Box */}
+          <Box
+            w="80%"
+            maxW="350px"
+            aspectRatio="9/16"
+            position="relative"
+            overflow="hidden"
+            rounded="2xl"
+            bg="black"
+          >
+            {fileData ? (
+              <Image
+                src={fileData.gifData}
+                alt="GIF"
+                objectFit="fit"
+                w="100%"
+                h="100%"
+              />
+            ) : (
+              <Text color="white" fontSize="lg" textAlign="center">
+                Loading...
+              </Text>
+            )}
+          </Box>
 
-    return (
-        <Box w="100%" h="100vh" bg="black" justifyContent={"center"} alignContent={"center"}>
-            {/* <VerticalDivider /> */}
-            {/* Video Content */}
-            <Center>
-                <Flex dir={"row"} align={"center"}>
-                        <Box position="relative">
-                            <Image
-                                rounded="md" src={fileData}
-                                alt="Example GIF"
-                                marginTop="40%"
-                            />
-                        </Box>
-                    {/* Scroll Buttons */}
-                    <VStack>
-                        <ScrollUp setIndex={setIndex} index={index} />
-                        <ScrollDown setIndex={setIndex} index={index} />
-                        {/* Like & Dislike Buttons */}
-                        <LikeButton />
-                        <DislikeButton />
-                        <CommentButton />
-                    </VStack>
-                </Flex>
-            </Center>
-        </Box>
-    );
+          {/* Action Buttons */}
+          <VStack spacing={5} align="center">
+            <ScrollUp setIndex={setIndex} index={index} />
+            <ScrollDown setIndex={setIndex} index={index} />
+            <LikeButton />
+            <DislikeButton />
+          </VStack>
+        </Flex>
+      </Center>
+    </Box>
+  );
 }
 
 // Scroll Up Button
 function ScrollUp({ setIndex, index }) {
-    const onClick = () => {
-        setIndex(index + 1)
-    }
-    console.log(typeof (setIndex))
-    return (
-        <IconButton
-            aria-label="Scroll Up"
-            icon={<ArrowUpIcon boxSize="2rem" />}
-            size="lg"
-            colorScheme="Gray" // Valid color scheme
-            onClick={onClick}
-        />
-    );
+  const onClick = () => {
+    setIndex(index + 1);
+  };
+  return (
+    <IconButton
+      aria-label="Scroll Up"
+      icon={<ArrowUpIcon />}
+      size="lg"
+      colorScheme="gray"
+      variant="outline"
+      onClick={onClick}
+    />
+  );
 }
 
 // Scroll Down Button
 function ScrollDown({ setIndex, index }) {
-    const onClick = () => {
-        if (index - 1 < 0) {
-            setIndex(0)
-        } else {
-            setIndex(index - 1)
-        }
-    }
-    return (
-        <IconButton
-            aria-label="Scroll Down"
-            icon={<ArrowDownIcon boxSize="2rem" />}
-            size="lg"
-            colorScheme="white" // Valid color scheme
-            onClick={onClick}
-        />
-    );
+  const onClick = () => {
+    setIndex(index > 0 ? index - 1 : 0);
+  };
+  return (
+    <IconButton
+      aria-label="Scroll Down"
+      icon={<ArrowDownIcon />}
+      size="lg"
+      colorScheme="gray"
+      variant="outline"
+      onClick={onClick}
+    />
+  );
 }
 
 // Like Button
 function LikeButton() {
-    const [likes, setLikes] = useState(0); // Track the number of likes
+  const [likes, setLikes] = useState(0);
 
-    // Increment the like count
-    const handleLike = () => {
-        setLikes(likes + 1);
-    };
+  const handleLike = () => {
+    setLikes(likes + 1);
+  };
 
-    return (
-        <HStack 
-            spacing={1} // Adjust spacing between the number and button
-            align="center" // Center the items in the stack
-        >
-            {/* Display the number of likes */}
-
-            {/* Like Button */}
-            <IconButton
-                aria-label="Like Video"
-                icon={<RiThumbUpFill size="2rem" />}
-                size="lg"
-                colorScheme="Gray" // Changed to a valid color scheme
-                onClick={handleLike}
-            />
-            <Text color="white" fontSize="10px">{likes}</Text>
-
-        </HStack>
-    );
+  return (
+    <VStack spacing={1} align="center">
+      <IconButton
+        aria-label="Like Video"
+        icon={<RiThumbUpFill />}
+        size="lg"
+        colorScheme="white"
+        variant="solid"
+        onClick={handleLike}
+      />
+      <Text color="white" fontSize="sm">
+        {likes}
+      </Text>
+    </VStack>
+  );
 }
 
 // Dislike Button
 function DislikeButton() {
-    const [dislikes, setDislikes] = useState(0); //track # of dislikes
+  const [dislikes, setDislikes] = useState(0);
 
-    //increment
-    const handleDislike = () => {
-        setDislikes(dislikes + 1);
-    };
+  const handleDislike = () => {
+    setDislikes(dislikes + 1);
+  };
 
-    return (
-        <HStack spacing={1}>
-            {/* Like Count */}
-            {/* Like Button */}
-            <IconButton
-                aria-label="Dislike Video"
-                icon={<RiThumbDownFill size="2rem" />}
-                size="lg"
-                colorScheme="Gray"
-                onClick={handleDislike}
-            />
-            {/* Display the number of likes */}
-            <Text color="white" fontSize="10px">{dislikes}</Text>
-        </HStack>
-    );
-}
-
-function CommentButton() {
-    const [comments, setComments] = useState(0); //track # of comments
-
-    //increment
-    const handleComments = () => {
-        setComments(comments + 1);
-    }
-
-    return (
-        <HStack spacing={1}>
-
-            <IconButton
-                aria-label="Comment"
-                icon={<MdOutlineComment size="2rem" />}
-                size="lg"
-                colorScheme="Gray"
-                onClick={handleComments}
-            />
-            {/* Display the number of likes */}
-            <Text color="white" fontSize="10px">{comments}</Text>
-        </HStack>
-    );
+  return (
+    <VStack spacing={1} align="center">
+      <IconButton
+        aria-label="Dislike Video"
+        icon={<RiThumbDownFill />}
+        size="lg"
+        colorScheme="white"
+        variant="solid"
+        onClick={handleDislike}
+      />
+      <Text color="white" fontSize="sm">
+        {dislikes}
+      </Text>
+    </VStack>
+  );
 }
 
 export default VideoPlayer;
